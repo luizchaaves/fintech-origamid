7422c7e03fcb1cddec257a585c476229cd06a01d
Luiz Otávio <luizotavio0106@outlook.com>
2023-08-11 15:17:55 -0300

    style: add css properties in DateInput component

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,4 +1,25 @@
-import { ComponentProps } from 'react';
+import { CSSProperties, ComponentProps } from 'react';
+
+const generalStyle: CSSProperties = {
+  fontSize: '1rem',
+  color: 'var(--color-2)',
+  padding: 'var(--gap-s) .75rem',
+  backgroundColor: 'var(--color-4)',
+  borderRadius: 'var(--gap)',
+};
+
+const labelStyle: CSSProperties = {
+  display: 'block',
+  marginBottom: 'var(--gap-s)',
+  fontWeight: '600',
+  ...generalStyle,
+};
+
+const inputStyle: CSSProperties = {
+  border: 'none',
+  fontFamily: 'monospace',
+  ...generalStyle,
+};
 
 type IDateInput = ComponentProps<'input'> & {
   label: string;
@@ -8,8 +29,10 @@ type IDateInput = ComponentProps<'input'> & {
 const DateInput = ({ label, id, ...props }: IDateInput) => {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} name={id} type="date" {...props} />
+      <label style={labelStyle} htmlFor={id}>
+        {label}
+      </label>
+      <input style={inputStyle} id={id} name={id} type="date" {...props} />
     </div>
   );
 };
